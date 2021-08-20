ee92eff0f0e06452024abd0b5b6ac865b33412c5
jckuri <jckuri@gmail.com>
2021-08-20 03:04:44 -0500

    second commit

---
+++ download_dataset.sh
@@ -0,0 +1,5 @@
+cd ..
+wget https://github.com/jckuri/Object_Detection_in_Urban_Environment_DATASET/archive/refs/heads/main.zip
+unzip main.zip
+rm main.zip
+mv Object_Detection_in_Urban_Environment_DATASET-main/processed/ DATA
